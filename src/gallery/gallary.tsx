9c3c0448f6def3b76b22095b20d93b9d89e0cfaf
Zer0Stra1n <sandor.seth@gmail.com>
2021-02-21 16:42:02 -0500

    added image paint from the gallary request and models

--- src/gallery/gallary.tsx
+++ src/gallery/gallary.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 
-export class Gallery extends React.Component<{}, {}> {
+interface Movie {
+    id: string;
+    poster: string;
+}
+
+interface GallaryState {
+    error: string | null;
+    isLoaded: boolean;
+    items: Movie[];
+}
+
+export class Gallery extends React.Component<{}, GallaryState> {
     constructor(props: {}) {
         super(props);
         this.state = {
@@ -13,25 +24,42 @@ export class Gallery extends React.Component<{}, {}> {
     componentDidMount() {
         fetch('https://gtrtoph0d7.execute-api.us-east-1.amazonaws.com/dev/media')
             .then(res => res.json())
-            .then(result => {
+            .then((result: Movie[]) => {
                 this.setState({
                     isLoaded: true,
                     items: result
                 });
-                }, error => {
-                    this.setState({
-                        error: error,
-                        isLoaded: true
-                    })
-                }
+            }, error => {
+                this.setState({
+                    error: error.message,
+                    isLoaded: true
+                })
+            }
             )
     }
 
-    render () {
-        return (
-            <div>
-                <input type="text"></input>
-            </div>
-        );
+    render() {
+        const { error, isLoaded, items } = this.state
+        if (error) {
+            return <div>Error: {error}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <div>
+                    <div>
+                        <input type="text"></input>
+                    </div>
+                    <ul>
+                        {items.map(item => (
+                            <li key={item.id}>
+                                <img src={item.poster}></img>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+            );
+        }
     }
 }
